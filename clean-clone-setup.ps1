f3bab0cbd4522e09d1ceab3f822b9808b72ec167
adamsimsy <adamsimsy@gmail.com>
2020-03-29 15:56:30 +1100

    updated new setup instructions

--- clean-clone-setup.ps1
+++ clean-clone-setup.ps1
@@ -1,13 +1,15 @@
 #Install node if required http://nodejs.org/
 
+git clone git@github.com:Adamsimsy/adamsimsy.github.io.git
+git checkout -b source origin/source
+git submodule update --init --recursive --remote
 npm install
+
+#might be needed on new pc
 npm install hexo-cli -g
 npm install hexo-server --save
 set-executionpolicy remotesigned
-
-#setup theme - also have a fork of this.
 npm i -S hexo-generator-search hexo-generator-feed hexo-renderer-less hexo-autoprefixer hexo-generator-json-content
-git clone https://github.com/stkevintan/hexo-theme-material-flow themes/hexo-theme-material-flow
 
 #start server
 hexo server
